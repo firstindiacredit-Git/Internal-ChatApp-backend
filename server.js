142f169d266858c3d1fdfe2e9cde76d55426f91a
rizwan3749 <aarishidrisi3749x@gmail.com>
2025-10-08 11:26:23 +0530

    sinod

--- server.js
+++ server.js
@@ -35,7 +35,8 @@ app.use((req, res, next) => {
 
 // Middleware
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
 
 // Serve static files for profile images
 app.use("/uploads", express.static("uploads"));
